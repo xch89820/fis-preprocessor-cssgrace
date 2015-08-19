f0202a22fd07b7860a6ce2eabc74a2d4231a79ca
xuchenhui <xuchenhui@baidu.com>
2015-08-19 13:25:49 +0800

    增加修改的cssgrace

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * fis.baidu.com
  */
 
-var cssgrace = require('cssgrace');
+var cssgrace = require('./lib/cssgrace.js');
 module.exports = function (content, file, settings) {
     return cssgrace.pack(content, { from: settings.filename } );
 };
